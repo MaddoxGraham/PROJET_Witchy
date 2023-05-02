fb03403172fa410659d21292e64b298df20b95f3
MaddoxGraham <57898176+MaddoxGraham@users.noreply.github.com>
2023-05-02 16:34:36 +0200

    2/mai/2023

--- witchy/public/assets/js/main.js
+++ witchy/public/assets/js/main.js
@@ -28,47 +28,42 @@ for (let index = 0; index < catBtn.length; index++) {
 
 
 }
-const cards = document.querySelectorAll('.card');
 
-cards.forEach(card => {
-  const imagePaths = card.dataset.images;
+document.querySelectorAll('.card').forEach(card => {
+  const images = card.getAttribute('data-images').split(',');
+  console.log(images);
+  let currentImage = 0;
 
-  if (imagePaths) {
-    const paths = imagePaths.split(',');
-    const img = card.querySelector('img');
-    
-    // Précharger les images secondaires
-    paths.forEach(path => {
-      const img = new Image();
-      if(path.substring(0,4) === "http"){
-        img.src = path;
-      } else {
-        img.src = "{{ asset('assets/uploads/products/mini/300x300-' ~" + path + ") }}";
-        console.log(path);
-        console.log(img.src);
-      }
-      
-    });
+  const primaryImage = card.querySelector('.card-img-top');
+  const parentDiv = primaryImage.parentElement;
 
-    // Basculer entre les images
-    card.addEventListener('mouseover', () => {
-      if (paths.length > 1) {
-        const index = Math.floor(Math.random() * paths.length);
-        if(paths[index].substring(0,4)=== "http"){
-          img.src = paths[index];
-        } else {
-          img.src = "{{ asset('assets/uploads/products/mini/300x300-' ~" + paths[index] + ") }}";
-          console.log(paths[index]);
-          console.log(img.src);
+  // Ajout de la balise img pour la deuxième image
+  const secondaryImage = document.createElement('img');
+  secondaryImage.classList.add('d-block', 'h-100', 'd-inline-block', 'card-img-top', 'img-fluid', 'w-100');
+  parentDiv.insertBefore(secondaryImage, primaryImage.nextSibling);
 
-        }
-      }
-    });
-   
-    // Revenir à l'image principale
-    card.addEventListener('mouseout', () => {
-      img.src = paths[0];
-    });
-  }
+  // Affichage de la première image
+  primaryImage.src = images[0];
+
+  // Fonction pour changer l'image au survol
+  parentDiv.addEventListener('mouseenter', () => {
+    currentImage = (currentImage + 1) % images.length;
+    if (currentImage === 0) {
+      primaryImage.style.display = 'block';
+      secondaryImage.style.display = 'none';
+    } else {
+      primaryImage.style.display = 'none';
+      secondaryImage.style.display = 'block';
+      secondaryImage.src = images[currentImage];
+    }
+  });
+
+  // Fonction pour revenir à la première image en sortant du survol
+  parentDiv.addEventListener('mouseleave', () => {
+    currentImage = 0;
+    primaryImage.style.display = 'block';
+    secondaryImage.style.display = 'none';
+  });
 });
+
 
